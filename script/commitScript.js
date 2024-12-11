04371448eb7bb20a6a2c215208aba777aad8d9b6
RedoRojo <cristhiabnvictorrm@gmaill.com>
2024-12-11 08:03:10 -0400

    Caso de un primer commit corregido

--- script/commitScript.js
+++ script/commitScript.js
@@ -27,7 +27,7 @@ const updateJsonFile = (commitId, commitName) => {
         fs.writeFileSync(filePath, JSON.stringify(existingData, null, 2));
     } catch (err) {
         if (err.code === 'ENOENT') {
-            fs.writeFileSync(filePath, JSON.stringify({ commits: [data] }, null, 2));
+            fs.writeFileSync(filePath, JSON.stringify([data], null, 2));
         } else {
             console.error('Error updating JSON file:', err);
         }
