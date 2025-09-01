e7790f91cd0957584697f70b9a668e5ed92cc84e
Robert <robertortizarias@gmail.com>
2025-09-01 16:55:46 -0400

    second commit:saludo con nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,13 @@
-import sumar from "./sumador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const nameInput = document.querySelector('#nombre');
+const form = document.querySelector('#Saludador-form');
+const div = document.querySelector('#resultado-div');
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const name = nameInput.value;
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+
+  div.innerHTML = '<p>' + 'Hola ' + name + '</p>';
 });
